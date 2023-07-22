bd5d92417befb96679bd7ac708b55675e05df505
Ahmad Abid <abidnasratyar01@gmail.com>
2023-07-22 23:12:03 +0430

    Initial push.

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+const { colors } = require("tailwindcss/colors");
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        "2x1": "1360px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
+    colors: {
+      ...colors,
+      "light-gold": "#f5bc51",
+      "dark-gold": "#533519",
+    },
+  },
+  plugins: [require('tailwindcss-animate'), require('tailwindcss/typography')],
+};
